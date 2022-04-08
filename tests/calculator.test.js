5d76a021ffa8aa75ec2bd4bc825236b7924df73a
stolomeo <tolomeosebastian@gmail.com>
2022-04-07 23:35:56 -0400

    Actually finish calculator

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,15 @@ const calculator = require("../src/calculator");
 test("Add a and b", () => {
   expect(calculator.add(1, 1)).toBe(2);
 });
+
+test("Subtract a and b", () => {
+  expect(calculator.subtract(4, 3)).toBe(1);
+});
+
+test("Multiply a and b", () => {
+  expect(calculator.multiply(12, 12)).toBe(144);
+});
+
+test("Divide a and b", () => {
+  expect(calculator.divide(14, 2)).toBe(7);
+});
