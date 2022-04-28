2cb747de2fff56f9c66e20d511a196fd59d2904f
Sebastian Tolomeo <96708237+stolomeo@users.noreply.github.com>
2022-04-28 00:27:00 -0400

    Change test description

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,17 +1,17 @@
 const calculator = require("../src/calculator");
 
-test("Add a and b", () => {
+test("Adds 2 numbers", () => {
   expect(calculator.add(1, 1)).toBe(2);
 });
 
-test("Subtract a and b", () => {
+test("Subtracts 2 numbers", () => {
   expect(calculator.subtract(4, 3)).toBe(1);
 });
 
-test("Multiply a and b", () => {
+test("Multiplies 2 numbers", () => {
   expect(calculator.multiply(12, 12)).toBe(144);
 });
 
-test("Divide a and b", () => {
+test("Divides 2 numbers", () => {
   expect(calculator.divide(14, 2)).toBe(7);
 });
